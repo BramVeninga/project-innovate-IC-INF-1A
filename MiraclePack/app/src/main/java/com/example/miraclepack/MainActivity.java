5f722d9cb77504abea7bb7836b652cf2eb648165
HuubHamstra <huub.hamstra@student.nhlstenden.com>
2023-05-10 16:37:14 +0200

    Developed the base UI of the app

---
+++ MiraclePack/app/src/main/java/com/example/miraclepack/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.miraclepack;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.miraclepack.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.bag) {
+                replaceFragment(new BagFragment());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            } else if (itemId == R.id.settings) {
+                replaceFragment(new SettingsFragment());
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+
+        if (fragment instanceof HomeFragment) {
+            setTitle("Home");
+        } else if (fragment instanceof BagFragment) {
+            setTitle("Bag");
+        } else if (fragment instanceof ProfileFragment) {
+            setTitle("Profile");
+        } else if (fragment instanceof SettingsFragment) {
+            setTitle("Settings");
+        }
+    }
+}
