321695bd4ba4c5b88bbbe29ebe01b64fc10641c7
HuubHamstra <huub.hamstra@student.nhlstenden.com>
2023-05-22 09:53:28 +0200

    Implemented SQLite, calendar view and recyclerview for bag content

--- MiraclePack/app/src/main/java/com/example/miraclepack/BagFragment.java
+++ MiraclePack/app/src/main/java/com/example/miraclepack/BagFragment.java
@@ -1,16 +1,27 @@
 package com.example.miraclepack;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 public class BagFragment extends Fragment {
 
@@ -20,16 +31,38 @@ public class BagFragment extends Fragment {
         // Required empty public constructor
     }
 
+    MyDatabaseHelper myDB;
+    ArrayList<String> bagContentName, bagContentCompartment;
+    CustomAdapter customAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_bag, container, false);
 
-        Spinner spinner = view.findViewById(R.id.weekdaySpinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(requireContext(), R.array.weekdays, android.R.layout.simple_spinner_item);
+        Spinner weekDaySpinner = view.findViewById(R.id.weekdaySpinner);
+
+        List<String> weekDays = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+
+        for (int i = 0; i < 7; i++) {
+            String day = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
+            weekDays.add(day);
+            calendar.add(Calendar.DAY_OF_WEEK, 1);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, weekDays);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        weekDaySpinner.setAdapter(adapter);
+
+        Calendar today = Calendar.getInstance();
+        int currentDayOfWeek = today.get(Calendar.DAY_OF_WEEK);
+        int selectedPosition = currentDayOfWeek - 1;
+
+        weekDaySpinner.setSelection(selectedPosition);
+
 
         addBagContent = view.findViewById(R.id.addBagContent);
         addBagContent.setOnClickListener(new View.OnClickListener() {
@@ -42,4 +75,42 @@ public class BagFragment extends Fragment {
 
         return view;
     }
+
+    private void changeImageViewColor() {
+        ImageView imageView = getView().findViewById(R.id.bagStatus);
+
+        boolean isConditionTrue = true; // Condition
+
+        if (isConditionTrue) {
+            imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green), android.graphics.PorterDuff.Mode.SRC_IN);
+        } else {
+            imageView.clearColorFilter();
+        }
+    }
 }
+
+
+//        myDB = new MyDatabaseHelper(BagFragment.this);
+//        //
+//
+//        storeDataInArrays();
+//
+//        customAdapter = new CustomAdapter(BagFragment.this, );
+//        recyclerView.setAdapter(customAdapter);
+//        recyclerView.setLayoutManger(new LinearLayoutManager(BagFragment.this));
+//
+//    }
+
+
+
+//
+//    void storeDataInArrays() {
+//        Cursor cursor = myDB.readAllData();
+//        if(cursor.getCount() == 0) {
+//            Toast.makeText(this, "No data.", Toast.LENGTH_SHORT).show();
+//        }
+//        else {
+//            while (cursor.moveToNext()) {
+//
+//            }
+//        }
--- MiraclePack/app/src/main/java/com/example/miraclepack/MyDatabaseHelper.java
+++ MiraclePack/app/src/main/java/com/example/miraclepack/MyDatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.miraclepack;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
@@ -15,18 +16,19 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
     // Tables
-    private static final String TABLE_ITEM = "Item";
-    private static final String TABLE_CONFIGURATION = "Configuration";
-    private static final String TABLE_COMPARTMENT = "Compartment";
-    private static final String TABLE_CONFIGURATION_ITEM = "ConfigurationItem";
+    private static final String TABLE_ITEM = "item";
+    private static final String TABLE_CONFIG = "config";
+    private static final String TABLE_COMPARTMENT = "compartment";
+    private static final String TABLE_CONFIG_ITEM = "configItem";
 
     // Columns
+    private static final String COLUMN_ID = "id";
     private static final String COLUMN_NAME = "name";
     private static final String COLUMN_WEEKDAY = "weekDay";
-    private static final String COLUMN_CONFIGURATION_NAME = "configurationName";
-    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_COMPARTMENT_NUMBER = "compartmentNumber";
     private static final String COLUMN_DESCRIPTION = "description";
-    private static final String COLUMN_ITEM_NAME = "itemName";
+    private static final String COLUMN_ITEM_ID = "itemID";
+    private static final String COLUMN_CONFIG_ID = "configID";
     private static final String COLUMN_COMPARTMENT_ID = "compartmentID";
 
     public MyDatabaseHelper(@Nullable Context context) {
@@ -37,54 +39,66 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createItemTableQuery = "CREATE TABLE " + TABLE_ITEM + " (" +
-                COLUMN_NAME + " TEXT PRIMARY KEY)";
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NAME + " VARCHAR(255))";
         db.execSQL(createItemTableQuery);
 
-        String createConfigurationTableQuery = "CREATE TABLE " + TABLE_CONFIGURATION + " (" +
-                COLUMN_WEEKDAY + " TEXT, " +
-                COLUMN_CONFIGURATION_NAME + " TEXT PRIMARY KEY)";
-        db.execSQL(createConfigurationTableQuery);
+        String createConfigTableQuery = "CREATE TABLE " + TABLE_CONFIG + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_NAME + " VARCHAR(255), " +
+                COLUMN_WEEKDAY + " VARCHAR(255))";
+        db.execSQL(createConfigTableQuery);
 
         String createCompartmentTableQuery = "CREATE TABLE " + TABLE_COMPARTMENT + " (" +
-                COLUMN_ID + " INTEGER PRIMARY KEY, " +
-                COLUMN_DESCRIPTION + " TEXT)";
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_COMPARTMENT_NUMBER + " INT, " +
+                COLUMN_DESCRIPTION + " VARCHAR(255))";
         db.execSQL(createCompartmentTableQuery);
 
-        String createConfigurationItemTableQuery = "CREATE TABLE " + TABLE_CONFIGURATION_ITEM + " (" +
-                COLUMN_ITEM_NAME + " TEXT, " +
-                COLUMN_COMPARTMENT_ID + " INTEGER, " +
-                COLUMN_CONFIGURATION_NAME + " TEXT, " +
-                "PRIMARY KEY (" + COLUMN_ITEM_NAME + ", " + COLUMN_COMPARTMENT_ID + ", " + COLUMN_CONFIGURATION_NAME + "), " +
-                "FOREIGN KEY (" + COLUMN_ITEM_NAME + ") REFERENCES " + TABLE_ITEM + "(" + COLUMN_NAME + "), " +
-                "FOREIGN KEY (" + COLUMN_COMPARTMENT_ID + ") REFERENCES " + TABLE_COMPARTMENT + "(" + COLUMN_ID + "), " +
-                "FOREIGN KEY (" + COLUMN_CONFIGURATION_NAME + ") REFERENCES " + TABLE_CONFIGURATION + "(" + COLUMN_CONFIGURATION_NAME + "))";
-        db.execSQL(createConfigurationItemTableQuery);
+        String createConfigItemTableQuery = "CREATE TABLE " + TABLE_CONFIG_ITEM + " (" +
+                COLUMN_ITEM_ID + " INT, " +
+                COLUMN_CONFIG_ID + " INT, " +
+                COLUMN_COMPARTMENT_ID + " INT, " +
+                "FOREIGN KEY (" + COLUMN_ITEM_ID + ") REFERENCES " + TABLE_ITEM + "(" + COLUMN_ID + "), " +
+                "FOREIGN KEY (" + COLUMN_CONFIG_ID + ") REFERENCES " + TABLE_CONFIG + "(" + COLUMN_ID + "), " +
+                "FOREIGN KEY (" + COLUMN_COMPARTMENT_ID + ") REFERENCES " + TABLE_COMPARTMENT + "(" + COLUMN_ID + "))";
+        db.execSQL(createConfigItemTableQuery);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ITEM);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONFIGURATION);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONFIG_ITEM);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMPARTMENT);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONFIGURATION_ITEM);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONFIG);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ITEM);
         onCreate(db);
     }
 
-    void addBagContent(String itemName, String weekDay, int compartmentID) {
+    void addBagContent(int itemID, int configID, int compartmentID) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
 
-        cv.put(COLUMN_ITEM_NAME, itemName);
-        cv.put(COLUMN_WEEKDAY, weekDay);
+        cv.put(COLUMN_ITEM_ID, itemID);
+        cv.put(COLUMN_CONFIG_ID, configID);
         cv.put(COLUMN_COMPARTMENT_ID, compartmentID);
 
-        long result = db.insert(TABLE_CONFIGURATION_ITEM, null, cv);
+        long result = db.insert(TABLE_CONFIG_ITEM, null, cv);
 
-        if(result == -1) {
+        if (result == -1) {
             Toast.makeText(context, "Fout bij het toevoegen", Toast.LENGTH_SHORT).show();
-        }
-        else {
+        } else {
             Toast.makeText(context, "Succesvol toegevoegd!", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    Cursor readAllData() {
+        String query = "SELECT * FROM "; // Query to select all data
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db != null) {
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
     }
 }
