5335366eb047e87ee9c2ccdaa9b676b934529c4e
HuubHamstra <huub.hamstra@student.nhlstenden.com>
2023-05-26 12:13:33 +0200

    Added basic geofencing class with notifications

---
+++ MiraclePack/app/src/main/java/com/example/miraclepack/Geofence.java
@@ -0,0 +1,41 @@
+package com.example.miraclepack;
+
+import android.location.Location;
+
+public class Geofence {
+    private String name;
+    private double latitude;
+    private double longitude;
+    private float radius;
+
+    public Geofence(String name, double latitude, double longitude, float radius) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.radius = radius;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public Location getLocation() {
+        Location location = new Location("");
+        location.setLatitude(latitude);
+        location.setLongitude(longitude);
+        return location;
+    }
+}
+
