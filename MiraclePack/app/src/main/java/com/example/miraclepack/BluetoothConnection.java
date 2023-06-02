3c49fbb704cc777facb7cb8989fc9a756c72beaa
BramVeninga <bram.veninga@student.nhlstenden.com>
2023-06-02 09:48:00 +0200

    saving current progress on the design branch with the bluetooth connection before transfering to a diffrent branch for safer development.

--- MiraclePack/app/src/main/java/com/example/miraclepack/BluetoothConnection.java
+++ MiraclePack/app/src/main/java/com/example/miraclepack/BluetoothConnection.java
@@ -1,17 +1,27 @@
+package com.example.miraclepack;
+
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.miraclepack.R;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class BluetoothConnection extends AppCompatActivity {
 
@@ -20,11 +30,13 @@ public class BluetoothConnection extends AppCompatActivity {
     private BluetoothAdapter bluetoothAdapter;
     private BluetoothDevice bluetoothDevice;
     private BluetoothSocket bluetoothSocket;
+
     private InputStream inputStream;
     private OutputStream outputStream;
 
     private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    private static final String DEVICE_ADDRESS = "00:00:00:00:00:00"; // Replace with HC-05 address
+    public static final byte[] DEVICE_ADDRESS = {0x00,0x21,0x13,0x00,0x6C,0x7A}; //"00:21:13:00:6C:7A"; // Replace with HC-05 address
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,26 +44,35 @@ public class BluetoothConnection extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+//        connectToBluetooth();
+//        try {
+//            TimeUnit.SECONDS.sleep(5);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//        sendData("send;");
 
-        Button connectButton = findViewById(R.id.connect_button);
-        connectButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                connectToBluetooth();
-            }
-        });
+//        Button connectButton = findViewById(R.id.connect_button);
+//        connectButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                connectToBluetooth();
+//            }
+//        });
 
-        Button sendButton = findViewById(R.id.send_button);
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendData("Hello from Android!");
-            }
-        });
+//        //Button sendButton = findViewById(R.id.send_button);
+//        sendButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                sendData("Hello from Android!");
+//            }
+//        });
     }
 
-    private void connectToBluetooth() {
-        bluetoothDevice = bluetoothAdapter.getRemoteDevice(DEVICE_ADDRESS);
+    public void connectToBluetooth() {
+        Log.d("DEVICE_ADDRESS", DEVICE_ADDRESS.toString());
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothDevice = bluetoothAdapter.getRemoteDevice(new byte[] {0x00,0x21,0x13,0x00,0x6C,0x7A});
         ConnectBluetoothTask connectTask = new ConnectBluetoothTask();
         connectTask.execute();
     }
@@ -69,6 +90,23 @@ public class BluetoothConnection extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... params) {
             try {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return null;
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                        ActivityCompat.requestPermissions(this,
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                LOCATION_PERMISSION_REQUEST_CODE);
+                    }
+                }
                 bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(MY_UUID);
                 bluetoothSocket.connect();
                 inputStream = bluetoothSocket.getInputStream();
